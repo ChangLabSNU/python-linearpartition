b504d93d4abc50d104c1a3a0416ec0ce778eac84
Hyeshik Chang <hyeshik@snu.ac.kr>
2024-01-20 01:17:09 +0900

    Update usage and unit test.

--- README.md
+++ README.md
@@ -26,15 +26,24 @@ The seq parameter should be an RNA sequence in *uppercase* letters,
 and any `T` should be converted to `U` before passing it to the function.
 
 ```python
->>> import linearpartition
->>> seq = 'UGUCGGGUAGCUUAUCAGACUGAUGUUGACUGUUGAAUCUCAUGGCAACACCAGUCGAUGGGCUGUCUGACA'
->>> linearpartition.partition(seq)
-('((((((((((((((((.(((((.(((((.(((.(((...))))))))))).)))))))))))))))))))))', -34.6)
+>>> import linearpartition as lp
+>>> seq = 'UGUCGGGGUUGGCUGUCUGACA'
+>>> bpmtx, fe = lp.partition(seq)
+>>> fe
+-7.216465644007023
+>>> import pandas as pd
+>>> pd.DataFrame(bpmtx).sort_values('prob', ascending=False).head()
+    i   j      prob
+19  3  18  0.999201
+18  2  19  0.998801
+17  1  20  0.997717
+21  5  16  0.996692
+22  4  17  0.996508
 ```
 
-The function returns a tuple with two elements. The first element
-is the predicted MFE structure, and the second element is the free
-energy of the structure in kcal/mol.
+The function returns a tuple with two elements. The first element is the
+predicted base pairing probability, and the second element is the free
+energy of the ensemble structure in kcal/mol.
 
 ### Author
 
---
+++ tests/test_linearpartition.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+from linearpartition import partition
+
+class TestLinearPartition(TestCase):
+
+    seq1 = 'UGUCGGGUAGCUUAUCAGACUGAUGUUGACUGUUGAAUCUCAUGGCAACACCAGUCGAUGGGCUGUCUGACA'
+    probmtx1 = [
+        (0, 71, 0.5397), (1, 70, 0.9983), (2, 69, 0.9994), (3, 68, 0.9998),
+        (4, 67, 0.9982), (5, 66, 0.9993), (5, 67, 0.0002), (6, 65, 0.9061),
+        (6, 66, 0.0003), (7, 64, 0.9293), (8, 63, 0.9976), (9, 62, 0.9999),
+        (10, 61, 0.9999), (11, 60, 0.9977), (12, 59, 0.9955), (13, 58, 0.9979),
+        (14, 57, 0.9983), (15, 56, 0.9899), (17, 54, 0.0001), (17, 55, 0.9982),
+        (18, 54, 0.9995), (19, 53, 0.9998), (20, 52, 0.9993), (21, 50, 0.0011),
+        (21, 51, 0.9947), (23, 49, 0.9764), (24, 48, 0.9997), (25, 47, 0.9989),
+        (26, 46, 0.9986), (27, 45, 0.9975), (29, 43, 0.0271), (29, 44, 0.9506),
+        (30, 41, 0.0214), (30, 43, 0.9369), (30, 44, 0.0004), (30, 46, 0.0000),
+        (31, 38, 0.0007), (31, 40, 0.0248), (31, 42, 0.7542), (31, 45, 0.0001),
+        (32, 41, 0.4639), (32, 44, 0.0001), (33, 41, 0.4888), (34, 38, 0.0054),
+        (34, 39, 0.0122), (34, 40, 0.7858), (35, 39, 0.5950), (37, 44, 0.0001),
+        (38, 43, 0.0001)]
+    freeenergy1 = -36.0677
+
+    def test_1(self):
+        probmtx, freeenergy = partition(self.seq1)
+        for expected, actual in zip(self.probmtx1, sorted(probmtx.tolist())):
+            self.assertEqual(expected[:2], actual[:2])
+            self.assertAlmostEqual(expected[2], actual[2], places=4)
+        self.assertAlmostEqual(freeenergy, self.freeenergy1, places=4)
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
